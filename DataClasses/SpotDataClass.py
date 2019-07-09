2ff9072eeb5a9e29c71cb302ad97d2beb6cb5cf3
N.asahi <n.asahi.timecapsule@gmail.com>
2019-07-09 15:58:14 +0900

    spotdataクラスの編集、ルート表示をデータクラスを用いて行う

--- DataClasses/SpotDataClass.py
+++ DataClasses/SpotDataClass.py
@@ -1,8 +1,16 @@
 import dataclasses
+import math
+from pyproj import Geod
 
 
 @dataclasses.dataclass(frozen=True)
 class Spot:
     name: str
-    latitude: int = 0
-    longitude: int = 0
+    latitude: float
+    longitude: float
+    stay_time: int
+
+    def difference(self, Spot) -> float:
+        g = Geod(ellps='WGS84')
+        azimuth, back_azimuth, distance_2d = g.inv(self.longitude, self.latitude, Spot.longitude, Spot.latitude)
+        return distance_2d
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from BinarySearchTree import BinarySearchTree
 from BinarySearchTree.BinarySearchNode import BinarySearchNode
 from Dijkstra import SpotDijkstra
 from Dijkstra.DijkstraNode import DijkstraNode
+from DataClasses.SpotDataClass import Spot
+
+import json
+import numpy as np
+from flask import Flask, request
+
 
 
 def blt_test():
@@ -15,26 +21,96 @@ def blt_test():
 
     print(root.list())
 
-def list_swap(node_list):
+
+def list_swap(spots):
     nodes = []
-    for one in node_list:
-        nodes.append(DijkstraNode(40, one))
+    for index in range(len(spots)):
+        nodes.append(DijkstraNode(spots[index].stay_time, index))
     for node in nodes:
         for added_node in nodes:
             node.add_node(added_node, 2)
     root = SpotDijkstra(nodes[0])
     return root
 
-def dijkstra_test():
-    node_list = [1,2,3,4,5]
-    node_staytimes = [20,30,20,10,4]
-    node_edges = [10,0,11,11,1]
 
-    root = list_swap(node_list)
+def create_spots():
+    spot_list: list[list[str, float, float]] = [["公立はこだて未来大学", 41.8332672, 140.7606784],
+                                                ["五稜郭タワー", 41.8104462, 140.7445977],
+                                                ["五稜郭駅", 41.8108967, 140.7463324],
+                                                ["函館山", 41.7669551, 140.7175339],
+                                                ["木古内駅", 41.6700811, 140.4524394]]
+    spots: list[Spot] = []
+    for spot in spot_list:
+        spots.append(Spot(spot[0], spot[1], spot[2], 40))
+    return spots
 
-    lists = root.routes(2)
+
+def calc_route(routes, spots):
+    required_time = []
+    for _, route in enumerate(routes):
+        sum = 0
+        for index_, id in enumerate(route):
+            if id < len(spots) and index_ < len(route) - 1:
+                sum += spots[id].difference(spots[route[index_ + 1]])
+            if index_ == 5:
+                print(sum)
+        required_time.append(sum)
+    return required_time
+
+def max_index(required_time, n):
+    time_np = np.array(required_time)
+    # ソートはされていない上位k件のインデックス
+    unsorted_max_indices = np.argpartition(-time_np, n)[:n]
+    # 上位k件の値
+    y = time_np[unsorted_max_indices]
+    # 大きい順にソートし、インデックスを取得
+    indices = np.argsort(-y)
+    # 類似度上位k件のインデックス
+    return unsorted_max_indices[indices]
+
+
+def min_index(required_time, n):
+    time_np = np.array(required_time)
+    # ソートはされていない上位k件のインデックス
+    unsorted_max_indices = np.argpartition(time_np, n)[:n]
+    # 上位k件の値
+    y = time_np[unsorted_max_indices]
+    # 大きい順にソートし、インデックスを取得
+    indices = np.argsort(y)
+    # 類似度上位k件のインデックス
+    return unsorted_max_indices[indices]
+
+
+def get_route(spots, routes):
+    required_time = calc_route(routes, spots)
+
+    max_list = min_index(required_time, 4)
+
+    for index in max_list:
+        for id in routes[index]:
+            print(spots[id].name)
+        print("距離は" + str(required_time[index] / 1000) + "km")
     print("=-----------=")
-    for list in lists:
-        print(list)
 
-dijkstra_test()
+
+def dijkstra_test():
+    spots = create_spots()
+    root = list_swap(spots)
+
+    routes = root.routes(2)
+
+    get_route(spots, routes)
+
+
+"dijkstra_test()"
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    print(request.data.decode())
+    return "Hello World!"
+
+
+app.run(host="localhost", port=5000)
